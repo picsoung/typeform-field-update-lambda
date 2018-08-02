fc874301a1bbd0d16bede6759eb426f3a963c48a
Nicolas Grenié <nicolas.grenie@gmail.com>
2018-08-02 15:03:56 -0700

    first commit

---
+++ handler.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const { DateTime } = require('luxon');
+const tf = require('./typeform.js')
+
+module.exports.daySync = async (event, context, callback) => {
+  var now = DateTime.local();
+  var dates = []
+  var j = 1;
+
+  do{
+    var d = now.plus({days: j});
+    if(process.env.EXCLUDE_WEEKENDS==="true"){//exclude weekends
+      if(d.weekday === 6){ //saturday
+        console.log("saturday")
+        d = d.plus({days: 2});
+        j = j+ 2
+      }else if(d.weekday === 7){ //sunday
+        console.log("sunday")
+        d = d.plus({days: 1});
+        j = j+ 1
+      }
+    }
+    j = j+ 1
+    dates.push({label:d.toLocaleString(DateTime.DATE_HUGE)})
+  }while(dates.length<process.env.INTERVAL)
+  console.log(dates)
+
+  //Get typeform form def
+  const form_def = await tf.getFormDef(process.env.FORM_ID)
+  //Find field
+  const field = form_def.fields.find(f => f.id === process.env.FIELD_ID)
+  //Replace field value
+  field.properties.choices = dates
+
+  // puts new form def
+  console.log('fields', JSON.stringify(field))
+  tf.updateForm(process.env.TF_TOKEN, process.env.FORM_ID, form_def)
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Form updated',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
---
+++ typeform.js
@@ -0,0 +1,61 @@
+var axios = require('axios')
+
+exports.listForms = function(access_token, page_number, page_size){
+  var options = {
+    url: "https://api.typeform.com/forms",
+    method: "GET",
+    headers: {
+      "Authorization": 'Bearer ' + access_token
+    },
+    params: {
+      page: page_number || 1,
+      page_size: page_size || 10
+    }
+  }
+
+  return axios(options)
+    .then(function (parsedBody) {
+      return parsedBody.data.items;
+    })
+    .catch(function (err) {
+      console.log('err', err)
+    });
+}
+
+exports.getFormDef = function(form_id){
+  var options = {
+    url: "https://api.typeform.com/forms/"+form_id,
+    method: "GET",
+  }
+
+  return axios(options)
+    .then(function (parsedBody) {
+      return parsedBody.data;
+    })
+    .catch(function (err) {
+      console.log('err', err)
+    });
+}
+
+exports.updateForm = function(access_token, form_id, form_def) {
+  var options = {
+    url: "https://api.typeform.com/forms/"+form_id,
+    method: "PUT",
+    headers: {
+      "Authorization": 'Bearer ' + access_token
+    },
+    data: JSON.stringify(form_def)
+  }
+
+  return axios(options)
+    .then(function (parsedBody) {
+      return parsedBody.data;
+    })
+    .catch(function (err) {
+      console.log('error')
+      console.log(err.response.data);
+      console.log(err.response.status);
+      console.log(err.response.headers);
+    });
+
+}
